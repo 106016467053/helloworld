d997d9f06e19958c6238eb92d1ded647b0a4b3e0
Admin <Admin@Rajitha-CR>
2019-02-13 21:17:27 +0530

    firstprogram

---
+++ Test/src/TestClass.java
@@ -0,0 +1,16 @@
+class Task{
+	void Display() {
+		System.out.println("Hello..world...welcome");
+	}
+}
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Task t;
+		t=new Task();
+		t.Display();
+		
+	}
+
+}
